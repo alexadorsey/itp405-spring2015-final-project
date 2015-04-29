b3d7198c808ee8b108757dc1c86742ff24c96d63
alexadorsey <alexadorsey@hotmail.com>
2015-04-28 19:18:52 -0700

    Trying to fix heroku

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => env('DB_HOST', 'itp460.usc.edu'),
+			'database'  => env('DB_DATABASE', 'alexa'),
+			'username'  => env('DB_USERNAME', 'alexa'),
+			'password'  => env('DB_PASSWORD', 'dorsey'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
